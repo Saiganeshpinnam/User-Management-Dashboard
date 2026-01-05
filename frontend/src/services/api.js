c7015ba61189abbf6d28882ad7872f1a67f9a6cb
Ganesh <saiganeshpinnam41@gmail.com>
2026-01-05 22:14:14 +0530

    solution

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,9 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
+
+console.log("VITE_API_URL =>", import.meta.env.VITE_API_URL);
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+});
+
 export default api;
